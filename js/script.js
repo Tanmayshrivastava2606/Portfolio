d1b7ad827c4d935c709f8a3c1fa8eb18af238143
Tanmay Shrivastava <tanmayshrivastava715@gmail.com>
2026-07-22 20:46:10 +0530

    Added premium cursor glow effect

--- js/script.js
+++ js/script.js
@@ -256,3 +256,19 @@ function showToast(message,type){
     },3000);
 
 }
+/* ==========================================
+        PREMIUM CURSOR
+========================================== */
+
+const cursor = document.querySelector(".cursor");
+const blur = document.querySelector(".cursor-blur");
+
+document.addEventListener("mousemove",(e)=>{
+
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+
+    blur.style.left = e.clientX + "px";
+    blur.style.top = e.clientY + "px";
+
+});
